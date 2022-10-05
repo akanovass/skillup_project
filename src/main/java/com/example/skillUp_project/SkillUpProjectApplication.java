61f588c5a480c6605e69b63d77c4db6084fe2e39
akanovass <akanovashyryn0@gmail.com>
2022-10-05 16:21:09 +0600

    commit

---
+++ src/main/java/com/example/skillUp_project/SkillUpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillUp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillUpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillUpProjectApplication.class, args);
+	}
+
+}
